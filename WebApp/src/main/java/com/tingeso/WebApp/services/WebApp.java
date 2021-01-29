698fce094e90b492f625228c87a342403a54605c
ManachoM <manuel.manriquez.b@usach.cl>
2021-01-29 15:27:18 -0300

    configuración y cambios varios

--- WebApp/src/main/java/com/tingeso/WebApp/services/WebApp.java
+++ WebApp/src/main/java/com/tingeso/WebApp/services/WebApp.java
@@ -1,3 +1,5 @@
+package com.tingeso.WebApp.services;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -5,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin("*")
 public class WebApp {
-    @GetMapping(value = "/")
+    @GetMapping(value = "/hola")
     public String holaMundo(){
         return "Hola Mundo";
     }
