96d064db5cb6f112a4d07b6d8215f42090b7fcf3
ManachoM <manuel.manriquez.b@usach.cl>
2021-01-29 23:38:55 -0300

    ola 2

--- WebApp/src/main/java/com/tingeso/WebApp/services/WebApp.java
+++ WebApp/src/main/java/com/tingeso/WebApp/services/WebApp.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin("*")
 public class WebApp {
+
     
     @GetMapping(value = "/hola")
     public String holaMundo(){
